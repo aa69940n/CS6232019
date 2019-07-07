b075394b21044c3834d926bd58a714b047c25288
Alaa Awad <aa69940n@pace.edu>
2019-07-07 12:46:57 -0400

    empty main class for now

--- project/src/edu/pace/GroupProject.java
+++ project/src/edu/pace/GroupProject.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * @author aawad
+ * @author Alaa Awad
  * Sample of JDBC for MySQL
  *
  */
@@ -22,37 +22,5 @@ public class GroupProject {
      */
     public static void main(String args[]) throws SQLException, ClassNotFoundException {
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test?useTimezone=true&serverTimezone=UTC",
-                System.getenv("DBUSER"),
-                System.getenv("DBPASSWORD")
-        );
-        // For atomicity
-        conn.setAutoCommit(false);
-
-        // For isolation
-        conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-        Statement stmt = null;
-        try {
-            // create statement object
-            stmt = conn.createStatement();
-
-            // Either the 2 following inserts are executed, or none of them are. This is atomicity.
-            stmt.executeUpdate("insert into student values (5, 'stud1', 'addr1', 'A')");
-            stmt.executeUpdate("insert into student values (6, 'stud2', 'addr2', 'B')");
-        } catch (SQLException e) {
-            System.out.println("A SQLException was thrown: " + e.getMessage());
-            // For atomicity
-            conn.rollback();
-            stmt.close();
-            conn.close();
-            return;
-        }
-
-        conn.commit();
-        stmt.close();
-        conn.close();
     }
 }
