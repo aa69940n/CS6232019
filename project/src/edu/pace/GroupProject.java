5162a60e8c53cacae292c06cd10158ac16494b8f
Alaa Awad <aa69940n@pace.edu>
2019-07-07 21:24:06 -0400

    initial delete depot code

--- project/src/edu/pace/GroupProject.java
+++ project/src/edu/pace/GroupProject.java
@@ -18,9 +18,41 @@ public class GroupProject {
      * @param args
      * @throws ClassNotFoundException
      * @throws SQLException
-     * @throws ClassNotFoundException
      */
-    public static void main(String args[]) throws SQLException, ClassNotFoundException {
+    public static void main(String args[]) throws ClassNotFoundException, SQLException {
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test?useTimezone=true&serverTimezone=UTC",
+                System.getenv("DBUSER"),
+                System.getenv("DBPASSWORD")
+        );
+        // For atomicity
+        conn.setAutoCommit(false);
+
+        // For isolation
+        conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+        Statement stmt = null;
+        try {
+            // create statement object
+            stmt = conn.createStatement();
+
+            // The depot d1 is deleted from Depot and Stock.
+            stmt.executeUpdate("DELETE FROM Stock WHERE dep_id='d1'");
+            stmt.executeUpdate("DELETE FROM depot WHERE dep_id='d1'");
+
+        } catch (SQLException e) {
+            System.out.println("A SQLException was thrown: " + e.getMessage());
+
+            // we only commit if all transactions were successful. atomicity
+            conn.rollback();
+            stmt.close();
+            conn.close();
+            return;
+        }
+
+        conn.commit();
+        stmt.close();
+        conn.close();
     }
 }
