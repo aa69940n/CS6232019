95e9ba3fdad79a87cdd321fa7d9064cdfc9f669a
LMarino1 <43488759+LMarino1@users.noreply.github.com>
2019-07-09 00:43:56 -0400

    Add files via upload

--- project/src/edu/pace/GroupProject.java
+++ project/src/edu/pace/GroupProject.java
@@ -1,13 +1,14 @@
 package edu.pace;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSet;
 
 /**
  * @author Alaa Awad
+ * @author Leonard Marino
  * Sample of JDBC for MySQL
  *
  */
@@ -21,12 +22,10 @@ public class GroupProject {
      */
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test?useTimezone=true&serverTimezone=UTC",
-                System.getenv("DBUSER"),
-                System.getenv("DBPASSWORD")
-        );
+                "jdbc:mysql://localhost:3306/test?user=root&password=password");
+        
         // For atomicity
         conn.setAutoCommit(false);
 
@@ -36,10 +35,18 @@ public class GroupProject {
         try {
             // create statement object
             stmt = conn.createStatement();
-
+            
+            populateDB(stmt);
+            
+            //Query tables and print, before deletion 
+            queryAll(stmt);
+            
             // The depot d1 is deleted from Depot and Stock.
             stmt.executeUpdate("DELETE FROM Stock WHERE dep_id='d1'");
             stmt.executeUpdate("DELETE FROM depot WHERE dep_id='d1'");
+            
+            //Query tables and print, after deletion 
+            queryAll(stmt);
 
         } catch (SQLException e) {
             System.out.println("A SQLException was thrown: " + e.getMessage());
@@ -54,5 +61,86 @@ public class GroupProject {
         conn.commit();
         stmt.close();
         conn.close();
+    }
+    
+    /**
+     * Method for populating DB with the necessary tables and example data for this code demonstration to work properly
+     * @param stmt
+     * @throws SQLException
+     */
+    public static void populateDB(Statement stmt) throws SQLException{
+    	//delete any old tables
+    	stmt.executeUpdate("DROP TABLE IF EXISTS Stock CASCADE");
+		stmt.executeUpdate("DROP TABLE IF EXISTS Product CASCADE");
+		stmt.executeUpdate("DROP TABLE IF EXISTS Depot CASCADE");
+		
+		//create tables
+		//Product
+		stmt.executeUpdate("CREATE TABLE Product(prod_id CHAR(10), pname VARCHAR(30), price DECIMAL)");
+		stmt.executeUpdate("ALTER TABLE Product ADD CONSTRAINT pk_product PRIMARY KEY (prod_id)");
+		stmt.executeUpdate("ALTER TABLE Product ADD CONSTRAINT ck_product_price CHECK (price>0)");
+		
+		//Depot
+		stmt.executeUpdate("Create table Depot(dep_id CHAR(10), addr VARCHAR(30), volume INTEGER)");
+		stmt.executeUpdate("ALTER TABLE Depot ADD CONSTRAINT pk_depot PRIMARY KEY (dep_id)");
+		stmt.executeUpdate("ALTER TABLE Depot ADD CONSTRAINT ck_depot_volume CHECK (volume>0)");
+		
+		//Stock
+		stmt.executeUpdate("Create table Stock(prod_id CHAR(10), dep_id CHAR(10), quantity INTEGER)");
+		stmt.executeUpdate("ALTER TABLE Stock ADD CONSTRAINT pk_stock PRIMARY KEY (prod_id, dep_id)");
+		stmt.executeUpdate("ALTER TABLE Stock ADD CONSTRAINT fk_stock_prod FOREIGN KEY (prod_id) REFERENCES Product(prod_id)");
+		stmt.executeUpdate("ALTER TABLE Stock ADD CONSTRAINT fk_stock_depot FOREIGN KEY (dep_id) REFERENCES Depot(dep_id)");
+		
+		//does not have a contingency for ON UPDATE / DELETE, add?
+		
+		System.out.println("Tables created");
+		
+		//populate tables with sample data
+		stmt.executeUpdate("INSERT INTO Product (prod_id, pname, price) "
+				+ "Values ('p1', 'tape', 2.5), ('p2', 'tv', 250), ('p3', 'vcr', 80)");
+		stmt.executeUpdate("INSERT INTO Depot (dep_id, addr, volume) "
+				+ "Values ('d1', 'New York', 9000), ('d2', 'Syracuse', 6000), ('d4', 'New York', 2000)");
+		stmt.executeUpdate("INSERT INTO Stock (prod_id, dep_id, quantity) "
+				+ "Values ('p1', 'd1', 1000), ('p1', 'd2', -100), ('p1', 'd4', 1200), ('p3', 'd1', 3000), " 
+				+ "('p3', 'd4', 2000), ('p2', 'd4', 1500), ('p2', 'd1', -400), ('p2', 'd2', 2000)");
+		
+		System.out.println("Tables populated");
+    }
+    
+    /**
+     * Method for printing the ResultSet of a query in a table-like format
+     * @param r
+     * @throws SQLException
+     */
+    public static void printResultSet(ResultSet r) throws SQLException {
+    	while(r.next()) {
+    		System.out.println(r.getString(1)+"\t "+r.getString(2)+"\t "+r.getString(3));   		
+    	}   	
+    }
+    
+    /**
+     * method for querying all 3 tables and printing the results
+     * @param stmt
+     * @throws SQLException
+     */
+    public static void queryAll(Statement stmt) throws SQLException {
+        ResultSet rP = stmt.executeQuery("SELECT * FROM Product");
+        System.out.println();
+        System.out.println("Product:");
+        System.out.println("prod_id  " + "pname " + " price");
+        printResultSet(rP);
+        
+        ResultSet rD = stmt.executeQuery("SELECT * FROM Depot");
+        System.out.println();
+        System.out.println("Depot:");
+        System.out.println("dep_id  " + " addr " + "\t\t volume");
+        printResultSet(rD);
+        
+        ResultSet rS = stmt.executeQuery("SELECT * FROM Stock");
+        System.out.println();
+        System.out.println("Stock:");
+        System.out.println("prod_id  " + "dep_id " + " quantity");
+        printResultSet(rS);
+    	
     }
 }
